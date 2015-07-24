5378adb745dbbbd45154c31c89b799fbfba68a4b
auton1 <adam.auton@gmail.com>
2015-07-24 10:44:28 -0400

    Initial Commit

---
+++ rMCMC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rMCMC
+//
+//  Created by Adam Auton on 24/07/15.
+//  Copyright (c) 2015 Adam Auton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
